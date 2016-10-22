7e748f766ce7f81104b1681afd85a07445ebec7d
F0X <bustamante808@gmail.com>
2016-10-22 14:00:31 -0500

    Eliminación de las funcionalidades que no se necesitan

--- app/src/main/java/com/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
+++ app/src/main/java/com/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
@@ -25,12 +25,12 @@ import com.vuforia.samples.VuforiaSamples.R;
 // This activity starts activities which demonstrate the Vuforia features
 public class ActivityLauncher extends ListActivity
 {
-    
+
+    private String mClassToLaunchPackage;
+
     private String mActivities[] = { "Image Targets", "VuMark", "Cylinder Targets",
-            "Multi Targets", "User Defined Targets", "Object Reco", "Cloud Reco", "Text Reco",
-            "Frame Markers", "Virtual Buttons"};
-    
-    
+            "User Defined Targets"};
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -48,68 +48,32 @@ public class ActivityLauncher extends ListActivity
     
     
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id)
-    {
-        
-        Intent intent = new Intent(this, AboutScreen.class);
-        intent.putExtra("ABOUT_TEXT_TITLE", mActivities[position]);
-        
-        switch (position)
-        {
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        mClassToLaunchPackage = getPackageName();
+
+        Intent i = new Intent();
+
+        switch (position) {
+
             case 0:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.ImageTargets.ImageTargets");
-                intent.putExtra("ABOUT_TEXT", "ImageTargets/IT_about.html");
+                i.setClassName(mClassToLaunchPackage, mClassToLaunchPackage + ".app.ImageTargets.ImageTargets");
                 break;
             case 1:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                        "app.VuMark.VuMark");
-                intent.putExtra("ABOUT_TEXT", "VuMark/VM_about.html");
+                i.setClassName(mClassToLaunchPackage, mClassToLaunchPackage + ".app.VuMark.VuMark");
+                //intent.putExtra("ACTIVITY_TO_LAUNCH","app.VuMark.VuMark");
                 break;
             case 2:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.CylinderTargets.CylinderTargets");
-                intent.putExtra("ABOUT_TEXT", "CylinderTargets/CY_about.html");
+                i.setClassName(mClassToLaunchPackage, mClassToLaunchPackage + ".app.CylinderTargets.CylinderTargets");
+                //intent.putExtra("ACTIVITY_TO_LAUNCH","app.CylinderTargets.CylinderTargets");
                 break;
             case 3:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.MultiTargets.MultiTargets");
-                intent.putExtra("ABOUT_TEXT", "MultiTargets/MT_about.html");
+                i.setClassName(mClassToLaunchPackage, mClassToLaunchPackage + ".app.UserDefinedTargets.UserDefinedTargets");
+                //intent.putExtra("ACTIVITY_TO_LAUNCH","app.UserDefinedTargets.UserDefinedTargets");
                 break;
-            case 4:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.UserDefinedTargets.UserDefinedTargets");
-                intent.putExtra("ABOUT_TEXT",
-                    "UserDefinedTargets/UD_about.html");
-                break;
-            case 5:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.ObjectRecognition.ObjectTargets");
-                intent.putExtra("ABOUT_TEXT", "ObjectRecognition/OR_about.html");
-                break;
-            case 6:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.CloudRecognition.CloudReco");
-                intent.putExtra("ABOUT_TEXT", "CloudReco/CR_about.html");
-                break;
-            case 7:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.TextRecognition.TextReco");
-                intent.putExtra("ABOUT_TEXT", "TextReco/TR_about.html");
-                break;
-            case 8:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.FrameMarkers.FrameMarkers");
-                intent.putExtra("ABOUT_TEXT", "FrameMarkers/FM_about.html");
-                break;
-            case 9:
-                intent.putExtra("ACTIVITY_TO_LAUNCH",
-                    "app.VirtualButtons.VirtualButtons");
-                intent.putExtra("ABOUT_TEXT", "VirtualButtons/VB_about.html");
-                break;
+
         }
-        
-        startActivity(intent);
-        
+
+        startActivity(i);
     }
 }
